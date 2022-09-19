d8bcb8884b5310da7fb8c9f47c71a188317d0256
hwangtaeseung <hwangtaeseung@HwangMacPro.local>
2022-09-19 15:23:13 +0900

    change resource names

--- Day1/Ch05/terraform/terraform-codes/eks-cluster.tf
+++ Day1/Ch05/terraform/terraform-codes/eks-cluster.tf
@@ -1,19 +1,19 @@
-resource "aws_eks_cluster" "test-eks-cluster" {
+resource "aws_eks_cluster" "skt-user03-eks-cluster" {
 
   depends_on = [
-    aws_iam_role_policy_attachment.test-iam-policy-eks-cluster,
-    aws_iam_role_policy_attachment.test-iam-policy-eks-cluster-vpc,
+    aws_iam_role_policy_attachment.skt-user03-iam-policy-eks-cluster,
+    aws_iam_role_policy_attachment.skt-user03-iam-policy-eks-cluster-vpc,
   ]
 
   name     = var.cluster-name
-  role_arn = aws_iam_role.test-iam-role-eks-cluster.arn
+  role_arn = aws_iam_role.skt-user03-iam-role-eks-cluster.arn
   version = "1.23"
 
   enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
 
   vpc_config {
-    security_group_ids = [aws_security_group.test-sg-eks-cluster.id]
-    subnet_ids         = [aws_subnet.test-public-subnet1.id, aws_subnet.test-public-subnet3.id]
+    security_group_ids = [aws_security_group.skt-user03-sg-eks-cluster.id]
+    subnet_ids         = [aws_subnet.skt-user03-public-subnet1.id, aws_subnet.skt-user03-public-subnet3.id]
     endpoint_public_access = true
   }
 
--- Day1/Ch05/terraform/terraform-codes/internet_gateway.tf
+++ Day1/Ch05/terraform/terraform-codes/internet_gateway.tf
@@ -1,8 +1,8 @@
-resource "aws_internet_gateway" "test-internet-gateway" {
+resource "aws_internet_gateway" "skt-user03-internet-gateway" {
 
   depends_on = [
-    aws_vpc.test-vpc
+    aws_vpc.skt-user03-vpc
   ]
 
-  vpc_id = aws_vpc.test-vpc.id
+  vpc_id = aws_vpc.skt-user03-vpc.id
 }
